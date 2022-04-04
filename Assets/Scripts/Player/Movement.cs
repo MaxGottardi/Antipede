1989efafd02b23908c444df48e904f34702e283e
MaxGottardi <13959073@student.uts.edu.au>
2022-04-04 20:44:08 +1000

    made it actually good

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -11,7 +11,7 @@ public class Movement : MonoBehaviour
 
     private float dirY;
     private float dirX;
-    public float moveSpeed = 5f;
+    public float moveSpeed = 10f;
     private float dist;
 
     private float timer = 0;
@@ -21,13 +21,18 @@ public class Movement : MonoBehaviour
     public bool aHeld = false;
     public bool sHeld = false;
     public bool dHeld = false;
+
+    private int nextSegInt;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        nextSeg = GameObject.Find("1");
+        //nextSeg = GameObject.Find("1");
         fps = GameObject.FindGameObjectWithTag("FPS");
         fpsText = fps.GetComponent<TextMesh>();
+
+        nextSegInt = int.Parse(gameObject.name) + 1;
+        nextSeg = GameObject.Find(nextSegInt.ToString());
     }
 
     // Update is called once per frame
@@ -42,83 +47,37 @@ public class Movement : MonoBehaviour
             timer = 0;
             fpsCount = 0;
         }
-        AreButtonsHeld();
-        dist = Vector3.Distance(transform.position, nextSeg.transform.position);
-
-        //Updates the segment after the player
-        //Going UP
-        if (wHeld && dist >= 1.05f)
-        {
-            //Debug.Log(dist);
-            nextSeg.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 1);
-        }
-        //Going Down
-        if (sHeld && dist >= 1.05f)
-        {
-            //Debug.Log(dist);
-            nextSeg.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + 1);
-        }
-        //Going Left
-        if (aHeld && dist >= 1.05f)
-        {
-            //Debug.Log(dist);
-            nextSeg.transform.position = new Vector2(gameObject.transform.position.x + 1, gameObject.transform.position.y);
-        }
-        //Going Right
-        if (dHeld && dist >= 1.05f)
-        {
-            //Debug.Log(dist);
-            nextSeg.transform.position = new Vector2(gameObject.transform.position.x - 1, gameObject.transform.position.y);
-        }
-
         
+        
+
         dirY = Input.GetAxis("Vertical");
         dirX = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(dirX * moveSpeed, dirY * moveSpeed);
+
+        if (nextSeg != null)
+        {
+            UpdateNextSeg();
+        }
     }
 
-    private void AreButtonsHeld()
+    private void UpdateNextSeg()
     {
-        //Checks if W is being Held - Going Up
-        if (dirY > 0)
-        {
-            wHeld = true;
-        }
+        Vector3 dir = nextSeg.transform.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), Time.deltaTime * RotationSpeed);
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
-        if (dirY == 0)
-        {
-            wHeld = false;
-        }
-        //Checks if A is being Held - Going Left
-        if (dirX < 0)
-        {
-            aHeld = true;
-        }
+        float dist = Vector3.Distance(nextSeg.transform.position, transform.position);
 
-        if (dirX == 0)
+        if (dist >= 1.5)
         {
-            aHeld = false;
-        }
-        //Checks if S is being Held - Going Down
-        if (dirY < 0)
-        {
-            sHeld = true;
-        }
+            float moveDist = dist - 0.0416f;
+            float x = Mathf.Cos(angle * Mathf.Deg2Rad) * moveDist;
+            float y = Mathf.Sin(angle * Mathf.Deg2Rad) * moveDist;
 
-        if (dirY == 0)
-        {
-            sHeld = false;
-        }
-        //Checks if D is being Held - Going Right
-        if (dirX > 0)
-        {
-            dHeld = true;
+            nextSeg.transform.position = new Vector3(x, y, 0f) + transform.position;
 
-        }
-
-        if (dirX == 0)
-        {
-            dHeld = false;
         }
     }
+
 }
--- Assets/Scripts/Player/SegmentMovement.cs
+++ Assets/Scripts/Player/SegmentMovement.cs
@@ -21,36 +21,30 @@ public class SegmentMovement : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
+    {       
+        if (nextSeg != null)
         {
-            Debug.Log(nextSegInt);
+            UpdateNextSeg();
         }
+    }
 
-        dist = Vector3.Distance(transform.position, nextSeg.transform.position);
-        //Going Up
-        if (prevSeg.transform.position.y == transform.position.y + 1 && dist >= 1.05f)
+    private void UpdateNextSeg()
+    {
+        Vector3 dir = nextSeg.transform.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), Time.deltaTime * RotationSpeed);
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        float dist = Vector3.Distance(nextSeg.transform.position, transform.position);
+
+        if (dist >= 1.5)
         {
-            //Debug.Log(dist);
-            nextSeg.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 1);
-        }
-        //Going Down
-        if (prevSeg.transform.position.y == transform.position.y - 1 && dist >= 1.05f)
-        {
-            //Debug.Log(dist);
-            nextSeg.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + 1);
-        }
-        //Going Left
-        if (prevSeg.transform.position.x == transform.position.x - 1 && dist >= 1.05f)
-        {
-            //Debug.Log(dist);
-            nextSeg.transform.position = new Vector2(gameObject.transform.position.x + 1, gameObject.transform.position.y);
-        }
-        //Going Right
-        if (prevSeg.transform.position.x == transform.position.x + 1 && dist >= 1.05f)
-        {
-            //Debug.Log(dist);
-            nextSeg.transform.position = new Vector2(gameObject.transform.position.x - 1, gameObject.transform.position.y);
+            float moveDist = dist - 0.0416f;
+            float x = Mathf.Cos(angle * Mathf.Deg2Rad) * moveDist;
+            float y = Mathf.Sin(angle * Mathf.Deg2Rad) * moveDist;
+
+            nextSeg.transform.position = new Vector3(x, y, 0f) + transform.position;
+
         }
     }
 }
