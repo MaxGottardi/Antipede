a58a6d757b0551d242621cebc3185341f7826c57
DaveyD24 <69884791+DaveyD24@users.noreply.github.com>
2022-04-24 18:12:07 +1000

    SFXManager
    As a player I want appropriate sound effects for every performable action in the game so that i can be better immersed in the game

---
+++ Assets/Scripts/SFXManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour
+{
+
+    [SerializeField] AudioSource sourceMusic;
+    [SerializeField] AudioSource sourcePlayer;
+
+    [SerializeField] AudioClip menuMusic;
+    [SerializeField] AudioClip loadingMusic;
+    [SerializeField] AudioClip mainMusic;
+    [SerializeField] AudioClip caveMusic;
+    [SerializeField] AudioClip bossMusic;
+
+    [SerializeField] AudioClip larvaeSFX;
+    [SerializeField] AudioClip powerupSFX;
+    [SerializeField] AudioClip specialSFX;
+    [SerializeField] AudioClip shieldSFX;
+    [SerializeField] AudioClip damageSFX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EnterBoss()
+    {
+        sourceMusic.clip = bossMusic;
+        sourceMusic.Play();
+    }
+
+    public void EnterCave()
+    {
+        sourceMusic.clip = caveMusic;
+        sourceMusic.Play();
+    }
+
+    public void CollectLarvae()
+    {
+        sourcePlayer.clip = larvaeSFX;
+        sourcePlayer.Play();
+    }
+
+    public void CollectPowerup()
+    {
+        sourcePlayer.clip = powerupSFX;
+        sourcePlayer.Play();
+    }
+
+    public void CollectSpecial()
+    {
+        sourcePlayer.clip = specialSFX;
+        sourcePlayer.Play();
+    }
+
+    public void TakeDamage()
+    {
+        sourcePlayer.clip = damageSFX;
+        sourcePlayer.Play();
+    }
+
+}
