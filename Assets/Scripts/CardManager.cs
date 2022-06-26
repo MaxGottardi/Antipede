5f98aa121b51ce0f049c115852fab3d913d64a2b
WichaelMu <69779024+WichaelMu@users.noreply.github.com>
2022-06-26 10:21:16 +1000

    Fixed Warnings

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -5,8 +5,8 @@ using UnityEngine.UI;
 
 public class CardManager : MonoBehaviour
 {
-
-    [SerializeField] Canvas canvas;
+	[SerializeField] Canvas canvas;
+#if false
     [SerializeField] Image cardCenter;
     [SerializeField] GameObject card;
 
@@ -36,65 +36,73 @@ public class CardManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        card1Pos = new Vector3(-331.17f, 103f, 0);
-        card2Pos = new Vector3(-207.8f, 103, 0);
-        card3Pos = new Vector3(-82.7f, 103, 0);
-        card4Pos = new Vector3(41, 103, 0);
-        card5Pos = new Vector3(167.2f, 103, 0);
-        card6Pos = new Vector3(305.9f, 103, 0);
+	card1Pos = new Vector3(-331.17f, 103f, 0);
+	card2Pos = new Vector3(-207.8f, 103, 0);
+	card3Pos = new Vector3(-82.7f, 103, 0);
+	card4Pos = new Vector3(41, 103, 0);
+	card5Pos = new Vector3(167.2f, 103, 0);
+	card6Pos = new Vector3(305.9f, 103, 0);
 
-        canvas = canvas.GetComponent<Canvas>();
+	canvas = canvas.GetComponent<Canvas>();
     }
+#endif
 
-    // Update is called once per frame
-    void Update()
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.Tab))
+		{
+			ShowOverlay(true);
+			Time.timeScale = 0.5f;
+			Time.fixedDeltaTime = 0.02F * Time.timeScale;
+		}
+		else if (Input.GetKeyUp(KeyCode.Tab))
+		{
+			ShowOverlay(false);
+			Time.timeScale = 1;
+			Time.fixedDeltaTime = 0.02F;
+		}
+
+	}
+
+	void ShowOverlay(bool bShow)
+	{
+		canvas.gameObject.SetActive(bShow);
+	}
+
+#if false
+	public void DisplayOverlay()
     {
-        if (Input.GetKey(KeyCode.Tab))
-        {
-            DisplayOverlay();
-            Time.timeScale = 0.5f;
-            Time.fixedDeltaTime = 0.02F * Time.timeScale;
-        }
-        if (Input.GetKeyUp(KeyCode.Tab))
-        {
-            HideOverlay();
-            Time.timeScale = 1;
-            Time.fixedDeltaTime = 0.02F;
-        }
-
-    }
-
-    public void DisplayOverlay()
-    {
-        canvas.gameObject.SetActive(true);
+	canvas.gameObject.SetActive(true);
     }
 
     public void HideOverlay()
     {
-        canvas.gameObject.SetActive(false);
+	canvas.gameObject.SetActive(false);
     }
 
     public void CollectCard(int cardIndex)
     {
-        SetSlot(cardIndex);
+	SetSlot(cardIndex);
 
-        //Instantiate(card, new Vector3(0, 0, 0), Quaternion.identity);
-        //cardCenter.gameObject.SetActive(true);
+	//Instantiate(card, new Vector3(0, 0, 0), Quaternion.identity);
+	//cardCenter.gameObject.SetActive(true);
 
-        //Choose a card
-        
-        //Set card gameobject
+	//Choose a card
+	
+	//Set card gameobject
 
-        //Set image in canvas
+	//Set image in canvas
     }
 
     public void SetSlot(int cardIndex)
     {
-        if (!slot1_active)
-        {
-            //set active
-            //change slot1 image
+	if (!slot1_active)
+	{
+	    //set active
+	    //change slot1 image
 
-        }
+	}
     }
+#endif
 }
--- Assets/Scripts/Weapons/WeaponCardUI.cs
+++ Assets/Scripts/Weapons/WeaponCardUI.cs
@@ -187,7 +187,6 @@ public class WeaponCardUI : MonoBehaviour
 
 	void UpdateText(ref AttachmentUIInfo AUII)
 	{
-		Debug.Log("updatetext");
 		//AUII.Update(AUII.WeaponAttachment.Attachment.name + "\nx" + AUII.Remaining);
 		AUII.Update("x" + AUII.Remaining);
 	}
